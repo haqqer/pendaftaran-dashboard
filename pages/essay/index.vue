8db0d34f039545d7f85ef90ee1cfe4979f1f6056
wkwksama <mailpakgasing@gmail.com>
2018-07-02 00:22:39 +0700

    tampilan index kasih tambahan kampus sam alogo room

--- pages/essay/index.vue
+++ pages/essay/index.vue
@@ -12,7 +12,6 @@
       <v-card v-for="registrar in registrarItems" :key="registrar.id" class="mb-4">
         <v-card-title>
           <v-avatar
-            tile
             size="48px"
             color="grey"
             class="mr-3">
@@ -23,17 +22,27 @@
               {{ registrar.fullname }}
               <v-icon :color="registrar.gender == 'male' ? 'info' : 'pink'">{{ iconGender(registrar.gender) }}</v-icon>
             </h3>
-            <div style="text-transform: capitalize;">{{ registrar.city.toLowerCase() }}</div>
             <v-btn v-if="registrar.socmed.instagram" small icon color="primary" :href="'https://instagram.com/' + registrar.socmed.instagram" target="_blank">
               <v-icon>fab fa-instagram</v-icon>
             </v-btn>
             <v-btn v-if="registrar.socmed.line" small icon color="success" :href="'http://line.me/ti/p/~' + registrar.socmed.line" target="_blank">
               <v-icon>fab fa-line</v-icon>
             </v-btn>
-            <div class="caption">{{ registrar.id }}</div>
+            <v-avatar
+              tile
+              size="38px"
+              color="grey"
+              class="mx-3">
+              <img :src="getRoomImageUrl(registrar.roomFirst)" alt="">
+            </v-avatar>
           </div>
         </v-card-title>
         <v-card-text>
+          <p style="text-transform: capitalize;">
+            {{ registrar.dateOfBirth | moment('from', 'now', true) }} ,
+            {{ registrar.institution.toLowerCase() }} ,
+            {{ registrar.city.toLowerCase() }}
+          </p>
           <strong>Motivasi Ikut FLS</strong>
           <small class="ml-2">( {{ wordCount(registrar.essayMotivationJoin) }} kata )</small>
           <p>{{ shortText(registrar.essayMotivationJoin) }}</p>
@@ -170,6 +179,24 @@ export default {
         console.log('--- error konfig ----')
         console.log(error.config);
       })
+    },
+    getRoomImageUrl (room) {
+      switch (room) {
+        case 'Education':
+          return 'https://user-images.githubusercontent.com/21119252/41973205-85ec42bc-7a3e-11e8-9a29-e3f296080e21.png'
+        case 'Digital':
+          return 'https://user-images.githubusercontent.com/21119252/41973182-71436b92-7a3e-11e8-9d7e-8f039c0e67e3.png'
+        case 'Poverty':
+          return 'https://user-images.githubusercontent.com/21119252/41973269-aa219768-7a3e-11e8-8e77-6023aef4d135.png'
+        case 'Human Capital':
+          return 'https://user-images.githubusercontent.com/21119252/41973250-a087b4e4-7a3e-11e8-845b-ec4c8c38d34f.png'
+        case 'Entrepreneurship':
+          return 'https://user-images.githubusercontent.com/21119252/41973233-91527996-7a3e-11e8-9b1c-34e2b8ee0118.png'
+        case 'Urban Planning':
+          return 'https://user-images.githubusercontent.com/21119252/41973340-e2cc96bc-7a3e-11e8-8a25-a079c0b6e279.png'
+        default:
+          return 'https://user-images.githubusercontent.com/21119252/41821836-c2787e10-7810-11e8-8d2a-cc829bea4ae3.png'
+      }
     }
   },
   created () {
