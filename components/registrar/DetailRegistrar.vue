961b29ce98d48f623eed850712150524ba33933f
wkwksama <mailpakgasing@gmail.com>
2018-07-03 23:28:28 +0700

    oke detail info pendaftar

---
+++ components/registrar/DetailRegistrar.vue
@@ -0,0 +1,196 @@
+<template>
+  <v-card v-if="registrar" class="mb-4">
+    <v-card-title class="pb-1">
+      <v-avatar
+        tile
+        size="48px"
+        color="grey"
+        class="mr-3">
+        <img :src="'https://img.devidentify.com/' + registrar.email" alt="">
+      </v-avatar>
+      <div>
+        <h3 class="title mb-0">
+          {{ registrar.fullname }}
+          <v-avatar
+            tile
+            size="38px"
+            color="grey"
+            class="mx-3">
+            <img :src="getRoomImageUrl(registrar.roomFirst)" alt="">
+          </v-avatar>
+        </h3>
+        <div style="text-transform: capitalize;">{{ registrar.institution.toLowerCase() }}</div>
+        <v-btn v-if="registrar.socmed.instagram" small icon color="primary" :href="'https://instagram.com/' + registrar.socmed.instagram" target="_blank">
+          <v-icon>fab fa-instagram</v-icon>
+        </v-btn>
+        <v-btn v-if="registrar.socmed.line" small icon color="success" :href="'http://line.me/ti/p/~' + registrar.socmed.line" target="_blank">
+          <v-icon>fab fa-line</v-icon>
+        </v-btn>
+      </div>
+    </v-card-title>
+    <v-card-text class="pt-0">
+      <v-tabs
+        slot="extension"
+        v-model="tabs"
+        centered
+        dark
+        class="mb-4"
+        icons-and-text
+        color="primary"
+        slider-color="white">
+        <v-tab
+          v-for="menu in tabMenu"
+          :key="menu.name">
+          {{ menu.name }}
+          <v-icon>{{menu.icon}}</v-icon>
+        </v-tab>
+        <v-tab-item>
+          <strong>Motivasi Ikut FLS</strong>
+          <small class="ml-2">( {{ wordCount(registrar.essayMotivationJoin) }} kata )</small>
+          <p>{{ registrar.essayMotivationJoin }}</p>
+
+          <strong>Alasan memilih room {{ registrar.roomFirst }}</strong>
+          <small class="ml-2">( {{ wordCount(registrar.essayRoomSelected) }} kata )</small>
+          <p>{{ registrar.essayRoomSelected }}</p>
+
+          <strong>Studi kasus room {{ registrar.roomFirst }}</strong>
+          <small class="ml-2">( {{ wordCount(registrar.essayCaseStudy) }} kata )</small>
+          <p>{{ registrar.essayCaseStudy }}</p>
+        </v-tab-item>
+        <v-tab-item>
+          <strong>Nama</strong>
+          <p>{{ registrar.fullname }} , dipanggil {{registrar.nickname}}</p>
+          <strong>Tempat Tanggal Lahir</strong>
+          <p>{{ registrar.placeOfBirth }} , {{ registrar.dateOfBirth | moment('D MMMM YYYY') }}</p>
+          <strong>Jenis Kelamin</strong>
+          <p>
+            {{ registrar.gender == 'male' ? 'Laki-laki' : 'Perempuan' }}
+            <v-icon :color="registrar.gender == 'male' ? 'info' : 'pink'">{{ iconGender(registrar.gender) }}</v-icon>
+          </p>
+          <strong>Alamat</strong>
+          <p>
+            {{ registrar.domicileAddress }}
+            <br> {{ registrar.city }}
+            <br> {{ registrar.province }}
+          </p>
+          <strong>Institusi</strong>
+          <p>{{ registrar.institution }}</p>
+          <strong>Kontak</strong>
+          <p>
+            {{ registrar.email }}
+            <br> {{ registrar.phone }}
+          </p>
+          <strong>Sosmed</strong>
+          <p>
+            <v-btn v-if="registrar.socmed.instagram" small icon color="primary" :href="'https://instagram.com/' + registrar.socmed.instagram" target="_blank">
+              <v-icon>fab fa-instagram</v-icon>
+            </v-btn>
+            {{ registrar.socmed.instagram }}
+            <br>
+            <v-btn v-if="registrar.socmed.line" small icon color="success" :href="'http://line.me/ti/p/~' + registrar.socmed.line" target="_blank">
+              <v-icon>fab fa-line</v-icon>
+            </v-btn>
+            {{ registrar.socmed.line }}
+          </p>
+        </v-tab-item>
+        <v-tab-item>
+          <div v-for="(achievement, i) in registrar.achievements" :key="i">
+            <strong>{{ achievement.name }}</strong>
+            <p>
+              {{ achievement.rank }}
+              <br> Tingkat {{ achievement.level }}
+              <br> {{ achievement.year }}
+            </p>
+          </div>
+        </v-tab-item>
+        <v-tab-item>
+          <div v-for="(organization, i) in registrar.organizations" :key="i">
+            <strong>{{ organization.name }}</strong>
+            <p>
+              {{ organization.position }}
+              <br> Tingkat {{ organization.level }}
+              <br> {{ organization.period }}
+            </p>
+          </div>
+        </v-tab-item>
+        <v-tab-item>
+          <div v-for="(socialActivity, i) in registrar.socialActivities" :key="i">
+            <strong>{{ socialActivity.name }}</strong>
+            <p>
+              Tingkat {{ socialActivity.level }}
+              <br> {{ socialActivity.year }}
+            </p>
+          </div>
+        </v-tab-item>
+      </v-tabs>
+
+
+      <v-divider></v-divider>
+
+      <h4 class="subheading">Nilai - </h4>
+      <div><strong>Prestasi</strong> {{ registrar.scoreAuto.achievement }}</div>
+      <div><strong>Organisasi</strong> {{ registrar.scoreAuto.organization }}</div>
+      <div><strong>Sosial</strong> {{ registrar.scoreAuto.socialActivity }}</div>
+      <div><strong>Essay</strong> {{ registrar.scoreEssay }}</div>
+      <div><strong>Total</strong> {{ registrar.scoreTotal }}</div>
+    </v-card-text>
+    <v-card-actions class="">
+
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    registrar: Object
+  },
+  data () {
+    return {
+      tabs: 0,
+      tabMenu: [
+        { icon: 'assignment', name: 'Essay' },
+        { icon: 'person', name: 'Bio' },
+        { icon: 'account_box', name: 'Prestasi' },
+        { icon: 'account_box', name: 'Organisasi' },
+        { icon: 'account_box', name: 'Aktifitas Sosial' },
+      ]
+    }
+  },
+  methods: {
+    iconGender (gender) {
+      switch (gender) {
+        case 'male':
+          return 'fas fa-mars'
+        case 'female':
+          return 'fas fa-venus'
+        default:
+          return 'fas fa-genderless'
+      }
+    },
+    wordCount (str) {
+      return str.split(' ')
+                .filter(function(n) { return n != '' })
+                .length;
+    },
+    getRoomImageUrl (room) {
+      switch (room) {
+        case 'Education':
+          return 'https://user-images.githubusercontent.com/21119252/41973205-85ec42bc-7a3e-11e8-9a29-e3f296080e21.png'
+        case 'Digital':
+          return 'https://user-images.githubusercontent.com/21119252/41973182-71436b92-7a3e-11e8-9d7e-8f039c0e67e3.png'
+        case 'Poverty':
+          return 'https://user-images.githubusercontent.com/21119252/41973269-aa219768-7a3e-11e8-8e77-6023aef4d135.png'
+        case 'Human Capital':
+          return 'https://user-images.githubusercontent.com/21119252/41973250-a087b4e4-7a3e-11e8-845b-ec4c8c38d34f.png'
+        case 'Entrepreneurship':
+          return 'https://user-images.githubusercontent.com/21119252/41973233-91527996-7a3e-11e8-9b1c-34e2b8ee0118.png'
+        case 'Urban Planning':
+          return 'https://user-images.githubusercontent.com/21119252/41973340-e2cc96bc-7a3e-11e8-8a25-a079c0b6e279.png'
+        default:
+          return 'https://user-images.githubusercontent.com/21119252/41821836-c2787e10-7810-11e8-8d2a-cc829bea4ae3.png'
+      }
+    }
+  }
+}
+</script>
--- pages/essay/_id.vue
+++ pages/essay/_id.vue
@@ -1,96 +1,7 @@
 <template>
   <v-layout v-if="registrar">
     <v-flex md8>
-      <v-card class="mb-4">
-        <v-card-title>
-          <v-avatar
-            tile
-            size="48px"
-            color="grey"
-            class="mr-3">
-            <img :src="'https://img.devidentify.com/' + registrar.email" alt="">
-          </v-avatar>
-          <div>
-            <h3 class="title mb-0">
-              {{ registrar.fullname }}
-              <v-avatar
-                tile
-                size="38px"
-                color="grey"
-                class="mx-3">
-                <img :src="getRoomImageUrl(registrar.roomFirst)" alt="">
-              </v-avatar>
-            </h3>
-            <div style="text-transform: capitalize;">{{ registrar.institution.toLowerCase() }}</div>
-            <v-btn v-if="registrar.socmed.instagram" small icon color="primary" :href="'https://instagram.com/' + registrar.socmed.instagram" target="_blank">
-              <v-icon>fab fa-instagram</v-icon>
-            </v-btn>
-            <v-btn v-if="registrar.socmed.line" small icon color="success" :href="'http://line.me/ti/p/~' + registrar.socmed.line" target="_blank">
-              <v-icon>fab fa-line</v-icon>
-            </v-btn>
-          </div>
-        </v-card-title>
-        <v-card-text>
-          <v-tabs
-            slot="extension"
-            v-model="tabs"
-            centered
-            dark
-            class="mb-4"
-            icons-and-text
-            color="primary"
-            slider-color="white">
-            <v-tab
-              v-for="menu in tabMenu"
-              :key="menu.name">
-              {{ menu.name }}
-              <v-icon>{{menu.icon}}</v-icon>
-            </v-tab>
-          </v-tabs>
-          <v-tab-items v-model="tabs">
-            <v-tab-item>
-              asdfasdf 1
-            </v-tab-item>
-            <v-tab-item>
-              asdfasdf 2
-            </v-tab-item>
-            <v-tab-item>
-              asdfasdf 3
-            </v-tab-item>
-            <v-tab-item>
-              asdfasdf 4
-            </v-tab-item>
-          </v-tab-items>
-          <p style="text-transform: capitalize;">
-            <v-icon :color="registrar.gender == 'male' ? 'info' : 'pink'">{{ iconGender(registrar.gender) }}</v-icon>
-            {{ registrar.dateOfBirth | moment('from', 'now', true) }} ,
-            Domisili {{ registrar.city.toLowerCase() }}
-          </p>
-          <strong>Motivasi Ikut FLS</strong>
-          <small class="ml-2">( {{ wordCount(registrar.essayMotivationJoin) }} kata )</small>
-          <p>{{ registrar.essayMotivationJoin }}</p>
-
-          <strong>Alasan memilih room {{ registrar.roomFirst }}</strong>
-          <small class="ml-2">( {{ wordCount(registrar.essayRoomSelected) }} kata )</small>
-          <p>{{ registrar.essayRoomSelected }}</p>
-
-          <strong>Studi kasus room {{ registrar.roomFirst }}</strong>
-          <small class="ml-2">( {{ wordCount(registrar.essayCaseStudy) }} kata )</small>
-          <p>{{ registrar.essayCaseStudy }}</p>
-
-          <v-divider></v-divider>
-
-          <h4 class="subheading">Nilai - </h4>
-          <div><strong>Prestasi</strong> {{ registrar.scoreAuto.achievement }}</div>
-          <div><strong>Organisasi</strong> {{ registrar.scoreAuto.organization }}</div>
-          <div><strong>Sosial</strong> {{ registrar.scoreAuto.socialActivity }}</div>
-          <div><strong>Essay</strong> {{ registrar.scoreEssay }}</div>
-          <div><strong>Total</strong> {{ registrar.scoreTotal }}</div>
-        </v-card-text>
-        <v-card-actions class="">
-
-        </v-card-actions>
-      </v-card>
+      <detail-registrar :registrar="registrar"></detail-registrar>
     </v-flex>
     <v-btn
       color="primary"
@@ -137,22 +48,16 @@
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import DetailRegistrar from '@/components/registrar/DetailRegistrar'
 
 export default {
   data () {
     return {
       registrar: null,
+      loadingRegistrar: false,
       dialog: false,
       loading: false,
       score: 0,
-      tabs: 0,
-      tabMenu: [
-        { icon: 'assignment', name: 'Essay' },
-        { icon: 'person', name: 'Bio' },
-        { icon: 'account_box', name: 'Prestasi' },
-        { icon: 'account_box', name: 'Organisasi' },
-        { icon: 'account_box', name: 'Aktifitas Sosial' },
-      ]
     }
   },
   computed: {
@@ -160,29 +65,15 @@ export default {
   },
   methods: {
     ...mapActions(['notify']),
-    iconGender (gender) {
-      switch (gender) {
-        case 'male':
-          return 'fas fa-mars'
-        case 'female':
-          return 'fas fa-venus'
-        default:
-          return 'fas fa-genderless'
-      }
-    },
-    wordCount (str) {
-      return str.split(' ')
-                .filter(function(n) { return n != '' })
-                .length;
-    },
     getDataRegistrar () {
+      this.loadingRegistrar = true
       this.$axios.$get('/registrars/' + this.$route.params.id, {
         params: {
         }
       }).then(response => {
         this.registrar = response
         console.log('registrar ', response)
-        // this.loadingRegistrar = false
+        this.loadingRegistrar = false
       }).catch(error => {
         console.log('--- awh error ----')
         if (error.response) {
@@ -204,6 +95,7 @@ export default {
           console.log('Error', error.message);
         }
         this.notify({ type: 'error', message: error.message })
+        this.loadingRegistrar = false
         console.log('--- error konfig ----')
         console.log(error.config);
       })
@@ -230,27 +122,11 @@ export default {
         this.loading = false
       })
     },
-    getRoomImageUrl (room) {
-      switch (room) {
-        case 'Education':
-          return 'https://user-images.githubusercontent.com/21119252/41973205-85ec42bc-7a3e-11e8-9a29-e3f296080e21.png'
-        case 'Digital':
-          return 'https://user-images.githubusercontent.com/21119252/41973182-71436b92-7a3e-11e8-9d7e-8f039c0e67e3.png'
-        case 'Poverty':
-          return 'https://user-images.githubusercontent.com/21119252/41973269-aa219768-7a3e-11e8-8e77-6023aef4d135.png'
-        case 'Human Capital':
-          return 'https://user-images.githubusercontent.com/21119252/41973250-a087b4e4-7a3e-11e8-845b-ec4c8c38d34f.png'
-        case 'Entrepreneurship':
-          return 'https://user-images.githubusercontent.com/21119252/41973233-91527996-7a3e-11e8-9b1c-34e2b8ee0118.png'
-        case 'Urban Planning':
-          return 'https://user-images.githubusercontent.com/21119252/41973340-e2cc96bc-7a3e-11e8-8a25-a079c0b6e279.png'
-        default:
-          return 'https://user-images.githubusercontent.com/21119252/41821836-c2787e10-7810-11e8-8d2a-cc829bea4ae3.png'
-      }
-    }
+
   },
   created () {
     this.getDataRegistrar()
-  }
+  },
+  components: { DetailRegistrar }
 }
 </script>
