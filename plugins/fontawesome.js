a1f346efdaa5d660472385b8ffacd8f82b393bea
haqqer <ilhamserbaguna@gmail.com>
2019-08-01 20:58:48 +0700

    add facebook and wa icon

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -3,10 +3,15 @@ import faMars from '@fortawesome/fontawesome-free-solid/faMars'
 import faVenus from '@fortawesome/fontawesome-free-solid/faVenus'
 import faGenderless from '@fortawesome/fontawesome-free-solid/faGenderless'
 import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram'
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebookF'
 import faLine from '@fortawesome/fontawesome-free-brands/faLine'
+import faWha from '@fortawesome/fontawesome-free-brands/faWhatsapp'
+
 
 fontawesome.library.add(faMars)
 fontawesome.library.add(faVenus)
 fontawesome.library.add(faGenderless)
 fontawesome.library.add(faInstagram)
+fontawesome.library.add(faFacebook)
 fontawesome.library.add(faLine)
+fontawesome.library.add(faWha)
