9683deec2e85a7f1fb8d39f923fc010ad3af15ac
haqqer <ilhamserbaguna@gmail.com>
2019-08-03 21:02:56 +0700

    update for main dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- <v-layout v-if="loading" justify-center>
+    <v-layout v-if="loading" justify-center>
       <v-progress-circular
         :size="70"
         :width="7"
@@ -16,14 +16,34 @@
               <h1 class="display-1 ml-4">{{ registrarSelected }} / {{ totalRegistrar }}</h1>
             </v-card-title>
             <v-card-text>
-              <p>Pendaftar dinilai</p>
+              <p>Pendaftar yang belum</p>
               <v-progress-linear v-model="percentScoredRegistrar"></v-progress-linear>
             </v-card-text>
           </v-card>
         </v-flex>
     </v-layout>
+    <v-layout class="mt-4 mb-5">
+      <v-flex md12 xs12>
+        <v-card>
+          <v-card-title>
+            <p class="subheading">Pendaftar Total : <strong>{{ totalRegistrar }}</strong></p>
+            <v-spacer></v-spacer>
+            <p>{{ new Date() | moment("dddd, MMMM Do YYYY, h:mm:ss a") }}</p>
+          </v-card-title>          
+        </v-card>
+      </v-flex>      
+    </v-layout>
     <v-layout class="mt-4">
-      <v-flex md12>
+      <v-flex md6 xs12>
+        <v-card>
+          <v-card-title>
+          </v-card-title>
+          <v-card-text>
+            <pie-chart :data="chartStatus" :download="true" :library="{plugins: { datalabels: {color: '#ffff'}}}"></pie-chart>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex md6 xs12>
         <v-card>
           <v-card-title>
             <p class="subheading">Pendaftar Total : <strong>{{ totalRegistrar }}</strong></p>
@@ -35,124 +55,142 @@
           </v-card-text>
         </v-card>
       </v-flex>
-    </v-layout> -->
+    </v-layout>
     <p>hello</p>
   </div>
 </template>
 
 <script>
-// import { mapActions } from 'vuex'
+import { mapActions } from 'vuex'
 
-// export default {
-//   data () {
-//     return {
-//       totalRegistrar: 0,
-//       registrarSelected: 0,
-//       loading: false,
-//       chartRoom: {}
-//     }
-//   },
-//   computed: {
-//     percentScoredRegistrar () {
-//       return this.registrarSelected / this.totalRegistrar * 100
-//     }
-//   },
-//   methods: {
-//     ...mapActions(['notify']),
-//     getTotalRegistrars () {
-//       this.loading = true
-//       this.$axios.$get('/registrars/count').then(response => {
-//         this.totalRegistrar = response.count
-//         console.log('total ', response)
-//         this.loading = false
-//       }).catch(error => {
-//         this.notify({ type: 'error', message: error.message })
-//       })
-//     },
-//     getRegistrarSelected () {
-//       this.loading = true
-//       this.$axios.$get('/registrars/count', {
-//         params: {
-//           where: { scoreEssay: { gt: 0 } }
-//         }
-//       }).then(response => {
-//         this.registrarSelected = response.count
-//         console.log('selected ', response)
-//         this.loading = false
-//       }).catch(error => {
-//         this.notify({ type: 'error', message: error.message })
-//       })
-//     },
-//     refreshRoomChart () {
-//       this.chartRoom = {}
-//       this.countHumanCapital()
-//       this.countEducation()
-//       this.countDigital()
-//       this.countUrbanPlanning()
-//       this.countEntrepreneurship()
-//       this.countPoverty()
-//     },
-//     countHumanCapital () {
-//       this.$axios.$get('/registrars/count', {
-//         params: { where: { roomFirst: 'Human Capital' } }
-//       }).then(response => {
-//         this.chartRoom = { ...this.chartRoom, 'Human Capital': response.count }
-//       }).catch(error => {
-//         this.notify({ type: 'error', message: error.message })
-//       })
-//     },
-//     countEducation () {
-//       this.$axios.$get('/registrars/count', {
-//         params: { where: { roomFirst: 'Education' } }
-//       }).then(response => {
-//         this.chartRoom = { ...this.chartRoom, 'Education': response.count }
-//       }).catch(error => {
-//         this.notify({ type: 'error', message: error.message })
-//       })
-//     },
-//     countDigital () {
-//       this.$axios.$get('/registrars/count', {
-//         params: { where: { roomFirst: 'Digital' } }
-//       }).then(response => {
-//         this.chartRoom = { ...this.chartRoom, 'Digital': response.count }
-//       }).catch(error => {
-//         this.notify({ type: 'error', message: error.message })
-//       })
-//     },
-//     countUrbanPlanning () {
-//       this.$axios.$get('/registrars/count', {
-//         params: { where: { roomFirst: 'Urban Planning' } }
-//       }).then(response => {
-//         this.chartRoom = { ...this.chartRoom, 'Urban Planning': response.count }
-//       }).catch(error => {
-//         this.notify({ type: 'error', message: error.message })
-//       })
-//     },
-//     countEntrepreneurship () {
-//       this.$axios.$get('/registrars/count', {
-//         params: { where: { roomFirst: 'Entrepreneurship' } }
-//       }).then(response => {
-//         this.chartRoom = { ...this.chartRoom, 'Entrepreneurship': response.count }
-//       }).catch(error => {
-//         this.notify({ type: 'error', message: error.message })
-//       })
-//     },
-//     countPoverty () {
-//       this.$axios.$get('/registrars/count', {
-//         params: { where: { roomFirst: 'Poverty' } }
-//       }).then(response => {
-//         this.chartRoom = { ...this.chartRoom, 'Poverty': response.count }
-//       }).catch(error => {
-//         this.notify({ type: 'error', message: error.message })
-//       })
-//     }
-//   },
-//   mounted () {
-//     this.getTotalRegistrars()
-//     this.getRegistrarSelected()
-//     this.$nextTick(() => {
-//       this.refreshRoomChart()
-//     })
-//   }
-// }
+export default {
+  data () {
+    return {
+      totalRegistrar: 0,
+      registrarSelected: 0,
+      loading: false,
+      chartRoom: {},
+      chartStatus: {}
+    }
+  },
+  computed: {
+    percentScoredRegistrar () {
+      return this.registrarSelected / this.totalRegistrar * 100
+    }
+  },
+  methods: {
+    ...mapActions(['notify']),
+    getTotalRegistrars () {
+      this.loading = true
+      this.$axios.$get('/delegates/count').then(response => {
+        console.log('total ', response.data)
+        this.totalRegistrar = response.data.count
+        this.loading = false
+      }).catch(error => {
+        this.notify({ type: 'error', message: error.message })
+      })
+    },
+    getRegistrarSelected () {
+      this.loading = true
+      this.$axios.$get('/delegates/count', {
+        params: {
+          status: 0
+        }
+      }).then(response => {
+        this.registrarSelected = response.data.count
+        console.log('selected ', response)
+        this.loading = false
+      }).catch(error => {
+        this.notify({ type: 'error', message: error.message })
+      })
+    },
+    refreshRoomChart () {
+      this.chartRoom = {}
+      this.countHumanCapital()
+      this.countEducation()
+      this.countDigital()
+      this.countUrbanPlanning()
+      this.countEntrepreneurship()
+      this.countPoverty()
+    },
+    countHumanCapital () {
+      this.$axios.$get('/delegates/count', {
+        params: {
+          room: 1
+        } 
+      }).then(response => {
+        this.chartRoom = { ...this.chartRoom, 'Digital': response.data.count }
+      }).catch(error => {
+        this.notify({ type: 'error', message: error.message })
+      })
+    },
+    countEducation () {
+      this.$axios.$get('/delegates/count', {
+        params: { room: 2 }
+      }).then(response => {
+        this.chartRoom = { ...this.chartRoom, 'Education': response.data.count }
+      }).catch(error => {
+        this.notify({ type: 'error', message: error.message })
+      })
+    },
+    countDigital () {
+      this.$axios.$get('/delegates/count', {
+        params: { room: 3 }
+      }).then(response => {
+        this.chartRoom = { ...this.chartRoom, 'Environment': response.data.count }
+      }).catch(error => {
+        this.notify({ type: 'error', message: error.message })
+      })
+    },
+    countUrbanPlanning () {
+      this.$axios.$get('/delegates/count', {
+        params: { room: 4 }
+      }).then(response => {
+        this.chartRoom = { ...this.chartRoom, 'International Relation': response.data.count }
+      }).catch(error => {
+        this.notify({ type: 'error', message: error.message })
+      })
+    },
+    countEntrepreneurship () {
+      this.$axios.$get('/delegates/count', {
+        params: { room: 5 }
+      }).then(response => {
+        this.chartRoom = { ...this.chartRoom, 'Technopreneur': response.data.count }
+      }).catch(error => {
+        this.notify({ type: 'error', message: error.message })
+      })
+    },
+    countPoverty () {
+      this.$axios.$get('/delegates/count', {
+        params: { room: 6 }
+      }).then(response => {
+        this.chartRoom = { ...this.chartRoom, 'Urban Planning': response.data.count }
+      }).catch(error => {
+        this.notify({ type: 'error', message: error.message })
+      })
+    },
+    async countStatus () {
+      const belumDinilai = await this.$axios.get('/delegates/count', {params: {status:0}});
+      const sudahDinilai = await this.$axios.get('/delegates/count', {params: {status:1}});
+      const waitingList = await this.$axios.get('/delegates/count', {params: {status:2}});
+      const diterima = await this.$axios.get('/delegates/count', {params: {status:3}});
+      this.chartStatus = {
+        'Belum dinilai':  belumDinilai.data.data.count,
+        'Sudah Dinilai':  sudahDinilai.data.data.count,
+        'Waiting List':  waitingList.data.data.count,
+        'Diterima':  diterima.data.data.count
+      }
+      console.log('belum dinilai ');
+      console.log(belumDinilai);
+    }
+  },
+  async mounted () {
+    this.getTotalRegistrars()
+    this.getRegistrarSelected()
+    this.$nextTick(() => {
+      this.refreshRoomChart()
+    })
+    await this.countStatus() 
+  }
+}
 </script>
